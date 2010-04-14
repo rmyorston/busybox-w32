4b393c73cbfd40c40dc40e609cf248883ce52573
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2010-04-14 02:12:01 +0200

    win32: Support certain ANSI sequences on cmd.exe
    Support sequences are mostly color ones.
    
    This was extracted from commit
    e56b799d6ad8afba4168fffa7218d44c041a72d2
    in Git repository. Changes from original version:
    
    > diff --git a/home/pclouds/w/git/compat/winansi.c b/tmp/winansi2.c
    > index 44dc293..e2e7010 100644
    > --- a/home/pclouds/w/git/compat/winansi.c
    > +++ b/tmp/winansi2.c
    > @@ -2,8 +2,9 @@
    >   * Copyright 2008 Peter Harris <git@peter.is-a-geek.org>
    >   */
    >
    > +#include "libbb.h"
    >  #include <windows.h>
    > -#include "../git-compat-util.h"
    > +#undef PACKED
    >
    >  /*
    >   Functions to be wrapped:
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>

--- include/mingw.h
+++ include/mingw.h
@@ -123,6 +123,17 @@ NOIMPL(sigfillset,int *mask UNUSED_PARAM);
 int fdprintf(int fd, const char *format, ...);
 
 /*
+ * ANSI emulation wrappers
+ */
+
+int winansi_fputs(const char *str, FILE *stream);
+int winansi_printf(const char *format, ...) __attribute__((format (printf, 1, 2)));
+int winansi_fprintf(FILE *stream, const char *format, ...) __attribute__((format (printf, 2, 3)));
+#define fputs winansi_fputs
+#define printf(...) winansi_printf(__VA_ARGS__)
+#define fprintf(...) winansi_fprintf(__VA_ARGS__)
+
+/*
  * stdlib.h
  */
 #define WIFEXITED(x) ((unsigned)(x) < 259)	/* STILL_ACTIVE */
