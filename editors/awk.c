1a3717342d6d3e774e4e2171eb8ea81945ec918e
Ron Yorston <rmy@pobox.com>
2014-01-13 17:58:06 +0000

    awk: treat \r as a space character to skip

--- editors/awk.c
+++ editors/awk.c
@@ -723,7 +723,11 @@ static char *skip_spaces(char *p)
 		if (*p == '\\' && p[1] == '\n') {
 			p++;
 			t_lineno++;
+#if !ENABLE_PLATFORM_MINGW32
 		} else if (*p != ' ' && *p != '\t') {
+#else
+		} else if (*p != ' ' && *p != '\t' && *p != '\r') {
+#endif
 			break;
 		}
 		p++;
