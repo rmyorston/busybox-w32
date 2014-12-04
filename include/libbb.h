df54ba3bd7ba7bdb98ca30a2a3393d2941d2f9a8
Jens Staal <staal1978@gmail.com>
2014-12-04 21:44:36 +0100

    change barriers to mfence

--- include/libbb.h
+++ include/libbb.h
@@ -1861,7 +1861,7 @@ extern struct globals *const ptr_to_globals;
 #define barrier() __asm__ __volatile__("":::"memory")
 #else
 extern void barrier( void );
-#pragma aux barrier = 0x0F 0xAE 0xF8;
+#pragma aux barrier = "mfence";
 #endif
 
 #define SET_PTR_TO_GLOBALS(x) do { \
