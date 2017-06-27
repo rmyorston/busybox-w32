8ec887a61f0f87ae1b5ca43ed562cfdd75a9c80b
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-06-27 15:40:52 +0200

    find_mount_point: avoid unused variables
    These variables are not used when compiling for MINGW.
    
    This change was forgotten in 878295063 (df: limited implementation for
    WIN32, 2013-04-03).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- libbb/find_mount_point.c
+++ libbb/find_mount_point.c
@@ -20,11 +20,12 @@
 struct mntent* FAST_FUNC find_mount_point(const char *name, int subdir_too)
 {
 	struct stat s;
-	FILE *mtab_fp;
 	struct mntent *mountEntry;
+#if !ENABLE_PLATFORM_MINGW32
+	FILE *mtab_fp;
 	dev_t devno_of_name;
 	bool block_dev;
-#if ENABLE_PLATFORM_MINGW32
+#else
 	static char mnt_fsname[4];
 	static char mnt_dir[4];
 	static struct mntent my_mount_entry = { mnt_fsname, mnt_dir, "", "", 0, 0 };
