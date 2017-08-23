47d5fa233ec73589fe8a198bcfad0a8efbe219de
Ron Yorston <rmy@pobox.com>
2017-08-23 11:18:19 +0100

    find_mount_point: avoid warning about unused parameter

--- include/mingw.h
+++ include/mingw.h
@@ -469,6 +469,8 @@ const char * next_path_sep(const char *path);
 #define has_dos_drive_prefix(path) (isalpha(*(path)) && (path)[1] == ':')
 #define is_absolute_path(path) ((path)[0] == '/' || (path)[0] == '\\' || has_dos_drive_prefix(path))
 
+#define find_mount_point(n, s) find_mount_point(n)
+
 /*
  * helpers
  */
