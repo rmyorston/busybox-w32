28ade29e98c3bf14fd2818684d2de0789efee312
Ron Yorston <rmy@pobox.com>
2018-05-13 08:27:02 +0100

    Update copyright date

--- libbb/appletlib.c
+++ libbb/appletlib.c
@@ -839,7 +839,7 @@ int busybox_main(int argc UNUSED_PARAM, char **argv)
 		}
 #endif
 		full_write2_str(
-			"BusyBox is copyrighted by many authors between 1998-2015.\n"
+			"BusyBox is copyrighted by many authors between 1998-2018.\n"
 			"Licensed under GPLv2. See source distribution for detailed\n"
 			"copyright notices.\n"
 			"\n"
