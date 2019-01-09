40d5dd07ea1f290eaed30a03fd598e33a8eaf495
Ron Yorston <rmy@pobox.com>
2019-01-09 08:23:35 +0000

    diff: omit unreliable test for same file
    diff tries to detect if it's been asked to compare a file with
    itself.  Because much of the information returned by stat(2) is
    fake (in particular, the inode number) this results in false positives.
    
    Simply omit the unreliable test.

--- editors/diff.c
+++ editors/diff.c
@@ -1039,7 +1039,7 @@ int diff_main(int argc UNUSED_PARAM, char **argv)
 	 * single NFS file system, if a local device number (st_dev) exceeds
 	 * 255, or if a local inode number (st_ino) exceeds 16777215.
 	 */
-	if (ENABLE_DESKTOP
+	if (ENABLE_DESKTOP && !ENABLE_PLATFORM_MINGW32
 	 && stb[0].st_ino == stb[1].st_ino
 	 && stb[0].st_dev == stb[1].st_dev
 	 && stb[0].st_size == stb[1].st_size
