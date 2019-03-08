rmyorston/busybox-w32

commit 585d17d262efabce4a9a87f33f531ef9ab7c0e36

win32: canonicalize path in chdir(2)
Provide an implementation of chdir(2) which canonicalizes the
path to resolve symlinks.  Otherwise changing to a symlinked
directory confuses 'ls -l' because it thinks '.' is a link
rather than a directory.

OTOH, using 'cd' in the shell to change to a symlinked directory
now results in a mismatch between the shell's idea of where we are
and what's displayed in the prompt.  But upstream BusyBox does
that too so it must be OK.

win32/mingw.c
@@ -1159,6 +1159,19 @@ int mingw_mkdir(const char *path, int mode UNUSED_PARAM)
 	return ret;
 }
 
+#undef chdir
+int mingw_chdir(const char *dirname)
+{
+	int ret = -1;
+	char *realdir = xmalloc_realpath(dirname);
+
+	if (realdir) {
+		ret = chdir(realdir);
+		free(realdir);
+	}
+	return ret;
+}
+
 #undef chmod
 int mingw_chmod(const char *path, int mode)
 {

include/mingw.h
@@ -271,9 +271,11 @@ mode_t mingw_umask(mode_t mode);
 IMPL(fchmod,int,0,int fildes UNUSED_PARAM, mode_t mode UNUSED_PARAM);
 NOIMPL(fchown,int fd UNUSED_PARAM, uid_t uid UNUSED_PARAM, gid_t gid UNUSED_PARAM);
 int mingw_mkdir(const char *path, int mode);
+int mingw_chdir(const char *path);
 int mingw_chmod(const char *path, int mode);
 
 #define mkdir mingw_mkdir
+#define chdir mingw_chdir
 #define chmod mingw_chmod
 
 #if ENABLE_LFS && !defined(__MINGW64_VERSION_MAJOR)

commit 9da32114e4779619b3cbbb4e3d795a5247e964d9

win32: extend normalisation of paths in realpath(3)
The code to normalise paths in resolve_symlinks(), which is used
by realpath(3), was incomplete and unable to handle UNC paths.
Make an ASCII version of normalize_ntpath() to extend the cases
covered.

This fixes a regression introduced by commit 585d17d26 (win32:
canonicalize path in chdir(2)):  it wasn't possible to change
to a directory with a UNC path.

win32/mingw.c
@@ -1001,6 +1001,25 @@ int link(const char *oldpath, const char *newpath)
 	return 0;
 }
 
+static char *normalize_ntpathA(char *buf)
+{
+	/* fix absolute path prefixes */
+	if (buf[0] == '\\') {
+		/* strip NT namespace prefixes */
+		if (!strncmp(buf, "\\??\\", 4) ||
+		    !strncmp(buf, "\\\\?\\", 4))
+			buf += 4;
+		else if (!strncasecmp(buf, "\\DosDevices\\", 12))
+			buf += 12;
+		/* replace remaining '...UNC\' with '\\' */
+		if (!strncasecmp(buf, "UNC\\", 4)) {
+			buf += 2;
+			*buf = '\\';
+		}
+	}
+	return buf;
+}
+
 static char *resolve_symlinks(char *path)
 {
 	HANDLE h;
@@ -1022,8 +1041,7 @@ static char *resolve_symlinks(char *path)
 		status = GetFinalPathNameByHandleA(h, path, MAX_PATH,
 							FILE_NAME_NORMALIZED|VOLUME_NAME_DOS);
 		if (status != 0 && status < MAX_PATH) {
-			/* skip '\\?\' prefix */
-			ptr = (!strncmp(path, "\\\\?\\", 4)) ? (path + 4) : path;
+			ptr = normalize_ntpathA(path);
 			goto end;
 		}
 	}