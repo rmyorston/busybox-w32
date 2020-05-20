rmyorston/busybox-w32

commit e6680912a298dc2dee83f41837cb52160cf562d9

dd: create a sparse file when seek=N is used
When the seek=N argument is used mark the file as sparse and set
the range that is sparse.

See https://stackoverflow.com/questions/4011508/how-to-create-a-sparse-file-on-ntfs

coreutils/dd.c
@@ -502,6 +502,9 @@ int dd_main(int argc UNUSED_PARAM, char **argv)
 	}
 	if (seek) {
 		size_t blocksz = (G.flags & FLAG_SEEK_BYTES) ? 1 : obs;
+#if ENABLE_PLATFORM_MINGW32
+		seek_sparse(ofd, seek * blocksz);
+#endif
 		if (lseek(ofd, seek * blocksz, SEEK_CUR) < 0)
 			goto die_outfile;
 	}

include/mingw.h
@@ -533,3 +533,4 @@ int chdir_system_drive(void);
 char *xabsolute_path(char *path);
 char *get_drive_cwd(const char *path, char *buffer, int size);
 void fix_path_case(char *path);
+void seek_sparse(int fd, size_t size);

win32/mingw.c
@@ -1750,3 +1750,20 @@ void fix_path_case(char *path)
 		}
 	}
 }
+
+void seek_sparse(int fd, size_t size)
+{
+	DWORD dwTemp;
+	HANDLE fh;
+	FILE_ZERO_DATA_INFORMATION fzdi;
+
+	if ((fh=(HANDLE)_get_osfhandle(fd)) == INVALID_HANDLE_VALUE)
+		return;
+
+	DeviceIoControl(fh, FSCTL_SET_SPARSE, NULL, 0, NULL, 0, &dwTemp, NULL);
+
+	fzdi.FileOffset.QuadPart = 0;
+	fzdi.BeyondFinalZero.QuadPart = size;
+	DeviceIoControl(fh, FSCTL_SET_ZERO_DATA, &fzdi, sizeof(fzdi),
+					 NULL, 0, &dwTemp, NULL);
+}

commit 184aa4b5d07d5f3fcc15dd4bb0d4636fba734de3

dd: fix handling of 'conv=notrunc seek=N'
Commit e6680912a (dd: create a sparse file when seek=N is used)
broke the use of 'conv=notrunc seek=N' to modify existing files.

Rename seek_sparse() to make_sparse() and:

- add an argument to specify the start of the sparse region;

- call make_sparse() before ftruncate();

- call make_sparse() only if:
  * we can determine the size of the file;
  * the file is not open in append mode;
  * the file is being extended.

This should fix GitHub issue #186.

coreutils/dd.c
@@ -466,6 +466,27 @@ int dd_main(int argc UNUSED_PARAM, char **argv)
 
 		xmove_fd(xopen(outfile, oflag), ofd);
 
+#if ENABLE_PLATFORM_MINGW32
+		{
+			off_t len = (off_t)seek * ((G.flags & FLAG_SEEK_BYTES) ? 1 : obs);
+			struct stat st;
+			int ret = fstat(ofd, &st);
+
+			if (ret == 0 && !(G.flags & FLAG_APPEND) && len > st.st_size)
+				make_sparse(ofd, st.st_size, len);
+
+			if (seek && !(G.flags & FLAG_NOTRUNC)) {
+				if (ftruncate(ofd, len) < 0) {
+					if (ret < 0
+					 || S_ISREG(st.st_mode)
+					 || S_ISDIR(st.st_mode)
+					) {
+						goto die_outfile;
+					}
+				}
+			}
+		}
+#else
 		if (seek && !(G.flags & FLAG_NOTRUNC)) {
 			size_t blocksz = (G.flags & FLAG_SEEK_BYTES) ? 1 : obs;
 			if (ftruncate(ofd, seek * blocksz) < 0) {
@@ -479,6 +500,7 @@ int dd_main(int argc UNUSED_PARAM, char **argv)
 				}
 			}
 		}
+#endif
 	} else {
 		outfile = bb_msg_standard_output;
 	}
@@ -502,9 +524,6 @@ int dd_main(int argc UNUSED_PARAM, char **argv)
 	}
 	if (seek) {
 		size_t blocksz = (G.flags & FLAG_SEEK_BYTES) ? 1 : obs;
-#if ENABLE_PLATFORM_MINGW32
-		seek_sparse(ofd, seek * blocksz);
-#endif
 		if (lseek(ofd, seek * blocksz, SEEK_CUR) < 0)
 			goto die_outfile;
 	}

include/mingw.h
@@ -537,5 +537,5 @@ int chdir_system_drive(void);
 char *xabsolute_path(char *path);
 char *get_drive_cwd(const char *path, char *buffer, int size);
 void fix_path_case(char *path);
-void seek_sparse(int fd, size_t size);
+void make_sparse(int fd, off_t start, off_t end);
 int skip_ansi_emulation(int reset);

win32/mingw.c
@@ -1751,7 +1751,7 @@ void fix_path_case(char *path)
 	}
 }
 
-void seek_sparse(int fd, size_t size)
+void make_sparse(int fd, off_t start, off_t end)
 {
 	DWORD dwTemp;
 	HANDLE fh;
@@ -1762,8 +1762,8 @@ void seek_sparse(int fd, size_t size)
 
 	DeviceIoControl(fh, FSCTL_SET_SPARSE, NULL, 0, NULL, 0, &dwTemp, NULL);
 
-	fzdi.FileOffset.QuadPart = 0;
-	fzdi.BeyondFinalZero.QuadPart = size;
+	fzdi.FileOffset.QuadPart = start;
+	fzdi.BeyondFinalZero.QuadPart = end;
 	DeviceIoControl(fh, FSCTL_SET_ZERO_DATA, &fzdi, sizeof(fzdi),
 					 NULL, 0, &dwTemp, NULL);
 }