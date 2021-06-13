f7e4fe39a69b82bcef1aaa68111ccfa68b209ada
Ron Yorston <rmy@pobox.com>
2021-06-13 08:13:25 +0100

    win32: add local dirent implementation
    Add a cut down version of the dirent implementation from git.
    The git developers said:
    
      The mingw-runtime implemenation of opendir, readdir and closedir
      sets errno to 0 on success, something that POSIX explicitly
      forbids.
    
    This also avoids having to link against libssp.a (commit 13eb34205)
    and reduces the size of the binary by 2KB.

---
+++ win32/dirent.c
@@ -0,0 +1,96 @@
+#include "libbb.h"
+
+struct DIR {
+	struct dirent dd_dir;
+	HANDLE dd_handle;     /* FindFirstFile handle */
+	int dd_stat;          /* 0-based index */
+};
+
+static inline void finddata2dirent(struct dirent *ent, WIN32_FIND_DATAA *fdata)
+{
+	/* copy file name from WIN32_FIND_DATA to dirent */
+	strcpy(ent->d_name, fdata->cFileName);
+}
+
+DIR *opendir(const char *name)
+{
+	char pattern[MAX_PATH];
+	WIN32_FIND_DATAA fdata;
+	HANDLE h;
+	int len;
+	DIR *dir;
+
+	/* check that name is not NULL */
+	if (!name) {
+		errno = EINVAL;
+		return NULL;
+	}
+	/* check that the pattern won't be too long for FindFirstFileA */
+	len = strlen(name);
+	if (len + 2 >= MAX_PATH) {
+		errno = ENAMETOOLONG;
+		return NULL;
+	}
+	/* copy name to temp buffer */
+	strcpy(pattern, name);
+
+	/* append optional '/' and wildcard '*' */
+	if (len && !is_dir_sep(pattern[len - 1]))
+		pattern[len++] = '/';
+	pattern[len++] = '*';
+	pattern[len] = 0;
+
+	/* open find handle */
+	h = FindFirstFileA(pattern, &fdata);
+	if (h == INVALID_HANDLE_VALUE) {
+		DWORD err = GetLastError();
+		errno = (err == ERROR_DIRECTORY) ? ENOTDIR : err_win_to_posix();
+		return NULL;
+	}
+
+	/* initialize DIR structure and copy first dir entry */
+	dir = xmalloc(sizeof(DIR));
+	dir->dd_handle = h;
+	dir->dd_stat = 0;
+	finddata2dirent(&dir->dd_dir, &fdata);
+	return dir;
+}
+
+struct dirent *readdir(DIR *dir)
+{
+	if (!dir) {
+		errno = EBADF; /* No set_errno for mingw */
+		return NULL;
+	}
+
+	/* if first entry, dirent has already been set up by opendir */
+	if (dir->dd_stat) {
+		/* get next entry and convert from WIN32_FIND_DATA to dirent */
+		WIN32_FIND_DATAA fdata;
+		if (FindNextFileA(dir->dd_handle, &fdata)) {
+			finddata2dirent(&dir->dd_dir, &fdata);
+		} else {
+			DWORD lasterr = GetLastError();
+			/* POSIX says you shouldn't set errno when readdir can't
+			   find any more files; so, if another error we leave it set. */
+			if (lasterr != ERROR_NO_MORE_FILES)
+				errno = err_win_to_posix();
+			return NULL;
+		}
+	}
+
+	++dir->dd_stat;
+	return &dir->dd_dir;
+}
+
+int closedir(DIR *dir)
+{
+	if (!dir) {
+		errno = EBADF;
+		return -1;
+	}
+
+	FindClose(dir->dd_handle);
+	free(dir);
+	return 0;
+}
---
+++ win32/dirent.h
@@ -0,0 +1,19 @@
+#ifndef DIRENT_H
+#define DIRENT_H
+
+typedef struct DIR DIR;
+
+#define DT_UNKNOWN 0
+#define DT_DIR     1
+#define DT_REG     2
+#define DT_LNK     3
+
+struct dirent {
+	char d_name[PATH_MAX];	// file name
+};
+
+DIR *opendir(const char *dirname);
+struct dirent *readdir(DIR *dir);
+int closedir(DIR *dir);
+
+#endif /* DIRENT_H */
