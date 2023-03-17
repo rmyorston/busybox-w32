rmyorston/busybox-w32

commit 93a63809f9146e39a1c36bdccdfb8ef3b7046c98

win32: add support for the euro currency symbol
The euro currency symbol was added to some OEM code pages.  See:

   https://www.aivosto.com/articles/charsets-codepages-dos.html

Add a configuration option (enabled by default) to support this.
When enabled:

- The read_key() function requests wide character key events.  This
  allows the euro symbol to be entered regardless of the console OEM
  code page, though it needs to be available in the ANSI code page.

- Conversions between OEM and ANSI code pages in winansi.c are
  modified to work around a bug in the Microsoft routines.

- If the OEM code page is 850 when BusyBox starts it's changed to
  858.  This is the only currently supported OEM code page.

Also, the shell read builtin is modified to use read_key() whenever
input is being taken from the console.

include/mingw.h
@@ -485,6 +485,7 @@ void qsort_string_vector_case(char **sv, unsigned count) FAST_FUNC;
 
 const char *get_busybox_exec_path(void);
 void init_winsock(void);
+void init_codepage(void);
 
 int has_bat_suffix(const char *p);
 int has_exe_suffix(const char *p);

libbb/appletlib.c
@@ -1221,6 +1221,9 @@ int main(int argc UNUSED_PARAM, char **argv)
 			break;
 		}
 	}
+# if ENABLE_FEATURE_EURO
+	init_codepage();
+# endif
 #endif
 
 #if defined(__MINGW64_VERSION_MAJOR)

win32/winansi.c
@@ -391,6 +391,74 @@ static const char *set_attr(const char *str)
 	return func + 1;
 }
 
+#if ENABLE_FEATURE_EURO
+void init_codepage(void)
+{
+	if (GetConsoleCP() == 850 && GetConsoleOutputCP() == 850) {
+		SetConsoleCP(858);
+		SetConsoleOutputCP(858);
+	}
+}
+
+static BOOL winansi_CharToOemBuff(LPCSTR s, LPSTR d, DWORD len)
+{
+	WCHAR *buf;
+	int i;
+
+	if (!s || !d)
+		return FALSE;
+
+	buf = xmalloc(len*sizeof(WCHAR));
+	MultiByteToWideChar(CP_ACP, 0, s, len, buf, len);
+	WideCharToMultiByte(CP_OEMCP, 0, buf, len, d, len, NULL, NULL);
+	if (GetConsoleOutputCP() == 858) {
+		for (i=0; i<len; ++i) {
+			if (buf[i] == 0x20ac) {
+				d[i] = 0xd5;
+			}
+		}
+	}
+	free(buf);
+	return TRUE;
+}
+
+static BOOL winansi_CharToOem(LPCSTR s, LPSTR d)
+{
+	if (!s || !d)
+		return FALSE;
+	return winansi_CharToOemBuff(s, d, strlen(s)+1);
+}
+
+static BOOL winansi_OemToCharBuff(LPCSTR s, LPSTR d, DWORD len)
+{
+	WCHAR *buf;
+	int i;
+
+	if (!s || !d)
+		return FALSE;
+
+	buf = xmalloc(len*sizeof(WCHAR));
+	MultiByteToWideChar(CP_OEMCP, 0, s, len, buf, len);
+	WideCharToMultiByte(CP_ACP, 0, buf, len, d, len, NULL, NULL);
+	if (GetConsoleOutputCP() == 858) {
+		for (i=0; i<len; ++i) {
+			if (buf[i] == 0x0131) {
+				d[i] = 0x80;
+			}
+		}
+	}
+	free(buf);
+	return TRUE;
+}
+
+# undef CharToOemBuff
+# undef CharToOem
+# undef OemToCharBuff
+# define CharToOemBuff winansi_CharToOemBuff
+# define CharToOem winansi_CharToOem
+# define OemToCharBuff winansi_OemToCharBuff
+#endif
+
 static int ansi_emulate(const char *s, FILE *stream)
 {
 	int rv = 0;

Config.in
@@ -454,6 +454,15 @@ config FEATURE_ICON_ALL
 
 endchoice
 
+config FEATURE_EURO
+	bool "Support the euro currency symbol (0.5 kb)"
+	default y
+	depends on PLATFORM_MINGW32
+	help
+	Support the entry and display of the euro currency symbol.  This
+	requires the OEM code page to be 858.  If the OEM code page of
+	the console is 850 when BusyBox starts it's changed to 858.
+
 comment 'Build Options'
 
 config STATIC

shell/shell_common.c
@@ -235,8 +235,7 @@ shell_builtin_read(struct builtin_read_params *params)
 		}
 #else
 		errno = 0;
-		if (isatty(fd) && (params->opt_n || params->opt_d || params->opt_t ||
-							(read_flags & BUILTIN_READ_SILENT))) {
+		if (isatty(fd)) {
 			int64_t key;
 
 			key = read_key(fd, NULL, timeout);

win32/termios.c
@@ -6,7 +6,12 @@ int64_t FAST_FUNC read_key(int fd, char *buf UNUSED_PARAM, int timeout)
 	INPUT_RECORD record;
 	DWORD nevent_out, mode;
 	int ret = -1;
+#if ENABLE_FEATURE_EURO
+	wchar_t uchar;
+	char achar;
+#else
 	char *s;
+#endif
 	int alt_pressed = FALSE;
 	DWORD state;
 
@@ -22,7 +27,11 @@ int64_t FAST_FUNC read_key(int fd, char *buf UNUSED_PARAM, int timeout)
 			if (WaitForSingleObject(cin, timeout) != WAIT_OBJECT_0)
 				goto done;
 		}
+#if ENABLE_FEATURE_EURO
+		if (!ReadConsoleInputW(cin, &record, 1, &nevent_out))
+#else
 		if (!ReadConsoleInput(cin, &record, 1, &nevent_out))
+#endif
 			goto done;
 
 		if (record.EventType != KEY_EVENT)
@@ -40,7 +49,11 @@ int64_t FAST_FUNC read_key(int fd, char *buf UNUSED_PARAM, int timeout)
 			alt_pressed = ((state & LEFT_ALT_PRESSED) != 0);
 		}
 
+#if ENABLE_FEATURE_EURO
+		if (!record.Event.KeyEvent.uChar.UnicodeChar) {
+#else
 		if (!record.Event.KeyEvent.uChar.AsciiChar) {
+#endif
 			if (alt_pressed) {
 				switch (record.Event.KeyEvent.wVirtualKeyCode) {
 				case VK_MENU:
@@ -91,11 +104,19 @@ int64_t FAST_FUNC read_key(int fd, char *buf UNUSED_PARAM, int timeout)
 			}
 			continue;
 		}
+#if ENABLE_FEATURE_EURO
+		uchar = record.Event.KeyEvent.uChar.UnicodeChar;
+		achar = uchar & 0x7f;
+		if (achar != uchar)
+			WideCharToMultiByte(CP_ACP, 0, &uchar, 1, &achar, 1, NULL, NULL);
+		ret = achar;
+#else
 		if ( (record.Event.KeyEvent.uChar.AsciiChar & 0x80) == 0x80 ) {
 			s = &record.Event.KeyEvent.uChar.AsciiChar;
 			OemToCharBuff(s, s, 1);
 		}
 		ret = record.Event.KeyEvent.uChar.AsciiChar;
+#endif
 		break;
 	}
  done:

commit cb50586aedc9725fb14dbb25263bba1598726037

win32: delay adjusting code page
Commit 93a63809f9 (win32: add support for the euro currency symbol)
caused all invocations of busybox-w32 to change code page 850 to
858.  This has been known to cause problems with fonts in PowerShell
(GitHub issue #207).

Delay changing the code page until an i/o operation is imminent.
Instances of PowerShell started by the `drop` applet during ssh login
thus no longer have their code page adjusted.

include/mingw.h
@@ -554,7 +554,6 @@ void qsort_string_vector_case(char **sv, unsigned count) FAST_FUNC;
 
 const char *get_busybox_exec_path(void);
 void init_winsock(void);
-void init_codepage(void);
 
 int has_bat_suffix(const char *p);
 int has_exe_suffix(const char *p);

libbb/appletlib.c
@@ -1290,9 +1290,7 @@ int main(int argc UNUSED_PARAM, char **argv)
 			break;
 		}
 	}
-# if ENABLE_FEATURE_EURO
-	init_codepage();
-# endif
+
 	/* Ignore critical errors, such as calling GetVolumeInformation() on
 	 * a floppy or CDROM drive with no media. */
 	SetErrorMode(SEM_FAILCRITICALERRORS);

win32/winansi.c
@@ -29,6 +29,10 @@
 static WORD plain_attr = 0xffff;
 static WORD current_attr;
 
+#if ENABLE_FEATURE_EURO
+static void init_codepage(void);
+#endif
+
 static HANDLE get_console(void)
 {
 	return GetStdHandle(STD_OUTPUT_HANDLE);
@@ -74,6 +78,11 @@ static int is_wine(void)
 int terminal_mode(int reset)
 {
 	static int mode = -1;
+
+#if ENABLE_FEATURE_EURO
+	if (mode < 0)
+		init_codepage();
+#endif
 
 	if (mode < 0 || reset) {
 		HANDLE h;
@@ -699,7 +708,7 @@ static char *process_escape(char *pos)
 }
 
 #if ENABLE_FEATURE_EURO
-void init_codepage(void)
+static void init_codepage(void)
 {
 	if (GetConsoleCP() == 850 && GetConsoleOutputCP() == 850) {
 		SetConsoleCP(858);
@@ -715,6 +724,7 @@ static BOOL winansi_CharToOemBuff(LPCSTR s, LPSTR d, DWORD len)
 	if (!s || !d)
 		return FALSE;
 
+	terminal_mode(FALSE);
 	buf = xmalloc(len*sizeof(WCHAR));
 	MultiByteToWideChar(CP_ACP, 0, s, len, buf, len);
 	WideCharToMultiByte(CP_OEMCP, 0, buf, len, d, len, NULL, NULL);
@@ -744,6 +754,7 @@ static BOOL winansi_OemToCharBuff(LPCSTR s, LPSTR d, DWORD len)
 	if (!s || !d)
 		return FALSE;
 
+	terminal_mode(FALSE);
 	buf = xmalloc(len*sizeof(WCHAR));
 	MultiByteToWideChar(CP_OEMCP, 0, s, len, buf, len);
 	WideCharToMultiByte(CP_ACP, 0, buf, len, d, len, NULL, NULL);