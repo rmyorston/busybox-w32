rmyorston/busybox-w32

commit 96c104a61c9a75c478eb6e830653a830d72fe8fb

ash: try harder to avoid ctrl-c issue
Commit 96a647690 (ash: prevent issue with ctrl-c and echo in loop)
attempted to fix the problem that interrupting a loop like:

   while true; do echo hello; done

caused the shell to exit.  However, it wasn't completely effective
and it only applied to echo and printf, not other builtins.

Revert 96a647690 and instead don't call raise_interrupt() from
crtl_handler() for the foreground interactive shell.

shell/ash.c
@@ -4788,7 +4788,7 @@ sprint_status48(char *os, int status, int sigonly)
 static BOOL WINAPI ctrl_handler(DWORD dwCtrlType)
 {
 	if (dwCtrlType == CTRL_C_EVENT || dwCtrlType == CTRL_BREAK_EVENT) {
-		if (!suppress_int)
+		if (!suppress_int && !(rootshell && iflag))
 			raise_interrupt(); /* does not return */
 		pending_int = 1;
 		return TRUE;
@@ -10970,34 +10970,11 @@ static int ulimitcmd(int, char **) FAST_FUNC;
 #define BUILTIN_SPEC_REG_ASSG   "7"
 
 /* Stubs for calling non-FAST_FUNC's */
-#if !ENABLE_PLATFORM_MINGW32
 #if ENABLE_ASH_ECHO
 static int FAST_FUNC echocmd(int argc, char **argv)   { return echo_main(argc, argv); }
 #endif
 #if ENABLE_ASH_PRINTF
 static int FAST_FUNC printfcmd(int argc, char **argv) { return printf_main(argc, argv); }
-#endif
-#else
-#if ENABLE_ASH_ECHO
-static int FAST_FUNC echocmd(int argc, char **argv)
-{
-	int ret;
-	INT_OFF;
-	ret = echo_main(argc, argv);
-	INT_ON;
-	return ret;
-}
-#endif
-#if ENABLE_ASH_PRINTF
-static int FAST_FUNC printfcmd(int argc, char **argv)
-{
-	int ret;
-	INT_OFF;
-	ret = printf_main(argc, argv);
-	INT_ON;
-	return ret;
-}
-#endif
 #endif
 #if ENABLE_ASH_TEST || BASH_TEST2
 static int FAST_FUNC testcmd(int argc, char **argv)   { return test_main(argc, argv); }

commit 490bbbb6e3a93045bb40442f5fc76d17a86b596b

ash: reinstate fix for ctrl-c issue
Reinstate the change introduced in commit 96c104a61c (ash: try harder
to avoid ctrl-c issue).  It was removed during recent work on traps.

shell/ash.c
@@ -4828,7 +4828,7 @@ static BOOL WINAPI ctrl_handler(DWORD dwCtrlType)
 # if ENABLE_FEATURE_EDITING
 		bb_got_signal = SIGINT; /* for read_line_input: "we got a signal" */
 # endif
-		if (!suppress_int)
+		if (!suppress_int && !(rootshell && iflag))
 			raise_interrupt();
 		pending_int = 1;
 		return TRUE;