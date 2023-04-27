rmyorston/busybox-w32

commit 99402ca92c77b13bee5181c129db18ecd9bf8938

xargs: kill children when interrupted by Ctrl-C
A user was running the following command in a clone of the
busybox-w32 repo from a busybox-w32 shell using Git for Windows:

   git rev-list --all | xargs git grep main.c

Using Ctrl-C to interrupt the command resulted in the familiar
problem that two processes ended up competing for user input:
in this case the busybox-w32 shell and the Git for Windows pager,
less.

Make the problem go away (in this particular case) by killing all
the children of xargs when it's interrupted.  This is probably a
sensible thing to do for other circumstances too.

Adds 64-100 bytes.

(GitHub issue #306)

findutils/xargs.c
@@ -121,9 +121,12 @@ struct globals {
 #if ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
 	int running_procs;
 	int max_procs;
-#if ENABLE_PLATFORM_MINGW32
+# if ENABLE_PLATFORM_MINGW32
 	HANDLE *procs;
+# endif
 #endif
+#if ENABLE_PLATFORM_MINGW32
+	pid_t pid;
 #endif
 	smalluint xargs_exitcode;
 #if ENABLE_FEATURE_XARGS_SUPPORT_QUOTES
@@ -188,6 +191,35 @@ enum {
 	IF_FEATURE_XARGS_SUPPORT_PARALLEL(    "P:+") \
 	IF_FEATURE_XARGS_SUPPORT_ARGS_FILE(   "a:")
 
+
+#if ENABLE_PLATFORM_MINGW32
+static BOOL WINAPI ctrl_handler(DWORD dwCtrlType)
+{
+	if (dwCtrlType == CTRL_C_EVENT || dwCtrlType == CTRL_BREAK_EVENT) {
+# if ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
+		if (G.max_procs == 1)
+# endif
+		{
+			if (G.pid > 0)
+				kill(-G.pid, SIGTERM);
+		}
+# if ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
+		else {
+			int i;
+
+			for (i = 0; i < G.running_procs; ++i) {
+				pid_t pid = GetProcessId(G.procs[i]);
+				if (pid > 0)
+					kill(-pid, SIGTERM);
+			}
+		}
+# endif
+		exit(128 + SIGINT);
+		return TRUE;
+	}
+	return FALSE;
+}
+#endif
 
 #if ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL && ENABLE_PLATFORM_MINGW32
 static int wait_for_slot(int *idx)
@@ -255,10 +287,11 @@ static int xargs_exec(void)
 #if !ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
 	status = spawn_and_wait(G.args);
 #else
-	if (G.max_procs == 1) {
-		status = spawn_and_wait(G.args);
-	} else {
 #if ENABLE_PLATFORM_MINGW32
+	if (G.max_procs == 1) {
+		G.pid = spawn(G.args);
+		status = G.pid < 0 ? -1 : wait4pid(G.pid);
+	} else {
 		int idx;
 		status = !G.running_procs && !G.max_procs ? 0 : wait_for_slot(&idx);
 		if (G.max_procs) {
@@ -275,6 +308,9 @@ static int xargs_exec(void)
 			}
 		}
 #else
+	if (G.max_procs == 1) {
+		status = spawn_and_wait(G.args);
+	} else {
 		pid_t pid;
 		int wstat;
  again:
@@ -723,6 +759,9 @@ int xargs_main(int argc UNUSED_PARAM, char **argv)
 
 	INIT_G();
 
+#if ENABLE_PLATFORM_MINGW32
+	SetConsoleCtrlHandler(ctrl_handler, TRUE);
+#endif
 	opt = getopt32long(argv, OPTION_STR,
 		"no-run-if-empty\0" No_argument "r",
 		&max_args, &max_chars, &G.eof_str, &G.eof_str

commit 8e6e5aa81c3bb7ce0b6a7f85ed0d01fc15604e5a

xargs: reinstate use of nofork applets
Commit 99402ca92 (xargs: kill children when interrupted by Ctrl-C)
caused all child processes to be spawned, losing the advantage of
running nofork applets without spawning.

Costs 64 bytes.

findutils/xargs.c
@@ -76,6 +76,8 @@
 
 #if ENABLE_PLATFORM_MINGW32
 #include <conio.h>
+#include "busybox.h"
+#include "NUM_APPLETS.h"
 #endif
 #include "libbb.h"
 #include "common_bufsiz.h"
@@ -282,31 +284,9 @@ static int xargs_exec(void)
 #if !ENABLE_PLATFORM_MINGW32
 	if (option_mask32 & OPT_STDIN_TTY)
 		xdup2(G.fd_tty, STDIN_FILENO);
-#endif
 
 #if !ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
 	status = spawn_and_wait(G.args);
-#else
-#if ENABLE_PLATFORM_MINGW32
-	if (G.max_procs == 1) {
-		G.pid = spawn(G.args);
-		status = G.pid < 0 ? -1 : wait4pid(G.pid);
-	} else {
-		int idx;
-		status = !G.running_procs && !G.max_procs ? 0 : wait_for_slot(&idx);
-		if (G.max_procs) {
-			HANDLE p = (HANDLE)mingw_spawn_proc((const char **)G.args);
-			if (p < 0)
-				status = -1;
-			else
-				G.procs[idx] = p;
-		} else {
-			while (G.running_procs) {
-				int status2 = wait_for_slot(&idx);
-				if (status2 && !status)
-					status = status2;
-			}
-		}
 #else
 	if (G.max_procs == 1) {
 		status = spawn_and_wait(G.args);
@@ -351,8 +331,44 @@ static int xargs_exec(void)
 			/* final waitpid() loop: must be ECHILD "no more children" */
 			status = 0;
 		}
-#endif
 	}
+#endif
+#endif
+
+#if ENABLE_PLATFORM_MINGW32
+# if ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
+	if (G.max_procs == 1) {
+# endif
+# if ENABLE_FEATURE_PREFER_APPLETS && (NUM_APPLETS > 1)
+		int applet = find_applet_by_name(G.args[0]);
+		if (applet >= 0 && APPLET_IS_NOFORK(applet)) {
+			status = run_nofork_applet(applet, G.args);
+		} else
+# endif
+		{
+			G.pid = spawn(G.args);
+			status = G.pid < 0 ? -1 : wait4pid(G.pid);
+		}
+# if ENABLE_FEATURE_XARGS_SUPPORT_PARALLEL
+	}
+	else {
+		int idx;
+		status = !G.running_procs && !G.max_procs ? 0 : wait_for_slot(&idx);
+		if (G.max_procs) {
+			HANDLE p = (HANDLE)mingw_spawn_proc((const char **)G.args);
+			if (p < 0)
+				status = -1;
+			else
+				G.procs[idx] = p;
+		} else {
+			while (G.running_procs) {
+				int status2 = wait_for_slot(&idx);
+				if (status2 && !status)
+					status = status2;
+			}
+		}
+	}
+# endif
 #endif
 	/* Manpage:
 	 * """xargs exits with the following status: