0efc74740ebc0d98af79ba4a5dfa73bfb5db3df0
Avi Halachmi (:avih) <avihpit@yahoo.com>
2023-06-27 14:41:47 +0300

    win32: support build with FEATURE_UNICODE_SUPPORT
    FEATURE_UTF8_MANIFEST enables Unicode args and filenames on Win 10+.
    
    FEATURE_UTF8_INPUT allows the shell prompt to digest correctly
    Unicode strings (as UTF8) which are typed or pasted.
    
    This commit adds support for building with FEATURE_UNICODE_SUPPORT
    (mostly by supporting 32 bit wchar_t which busybox expects):
    
    - Unicode-aware line-edit - for the most part cursor movement/del
      being (UTF8) codepoint-aware rather than assuming that one-byte
      equals one-char-on-screen.
    
    - Codepoint-aware operations in some other utils, like rev or wc -c.
    
    - When UNICODE_COMBINING_WCHARS and UNICODE_WIDE_WCHARS are enabled,
      some screen-width-aware operations, like with fold, ls, expand, etc.
    
    The busybox Unicode support is incomplete, and even less so with the
    builtin libc replacement functions, like wcwidth, which are active
    when UNICODE_USING_LOCALE is unset (mingw lacks those functions).
    
    FEATURE_CHECK_UNICODE_IN_ENV should be set so that Unicode is not
    hardcoded but rather depends on the ANSI codepage and some env vars:
    LC_ALL=C disables Unicode support, else it's enabled if ACP is UTF8.
    
    There's at least one known issue where the tab-completion-prefix-case
    is not updated correctly, e.g. ~/desk<tab> completes to ~/desktop/
    instead of ~/Desktop/, because the code which handles it exists
    only at the non-unicode code paths, but that's not very critical.
    
    That seems to be the only case where mingw-specific code is disabled
    when Unicode is enabled, but there could be other unknown issues.
    
    None of the Unicode options is enabled by default, and the next
    commit will make it easier to create a build which supports Unicode.

--- libbb/unicode.c
+++ libbb/unicode.c
@@ -69,8 +69,14 @@ void FAST_FUNC init_unicode(void)
 void FAST_FUNC reinit_unicode(const char *LANG)
 {
 	unicode_status = UNICODE_OFF;
+#if ENABLE_PLATFORM_MINGW32
+	/* enable unicode only when ACP is UTF8 and the env var is not 'C' */
+	if (GetACP() != CP_UTF8 || (LANG && LANG[0] == 'C' && LANG[1] == 0))
+		return;
+#else
 	if (!LANG || !(strstr(LANG, ".utf") || strstr(LANG, ".UTF")))
 		return;
+#endif
 	unicode_status = UNICODE_ON;
 }
 
