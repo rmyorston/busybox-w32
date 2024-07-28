rmyorston/busybox-w32

commit 4d8a277d59ad9c4036c9c595e0647a900f6bd7c8

Implement nonblocking wait

shell/ash.c
@@ -4079,7 +4079,7 @@ static BOOL WINAPI ctrl_handler(DWORD dwCtrlType)
  * They don't support waitpid(-1)
  */
 static pid_t
-waitpid_child(int *status)
+waitpid_child(int *status, int wait_flags)
 {
 	HANDLE *pidlist, *pidp;
 	int pid_nr = 0;
@@ -4111,7 +4111,8 @@ waitpid_child(int *status)
 	LOOP(*pidp++ = (HANDLE)ps->ps_pid);
 	#undef LOOP
 
-	idx = WaitForMultipleObjects(pid_nr, pidlist, FALSE, INFINITE);
+	idx = WaitForMultipleObjects(pid_nr, pidlist, FALSE,
+				wait_flags|WNOHANG ? 0 : INFINITE);
 	if (idx >= pid_nr) {
 		free(pidlist);
 		return -1;
@@ -4150,7 +4151,7 @@ dowait(int wait_flags, struct job *job)
 	if (doing_jobctl)
 		wait_flags |= WUNTRACED;
 #if ENABLE_PLATFORM_MINGW32
-	pid = waitpid_child(&status);
+	pid = waitpid_child(&status, wait_flags);
 #else
 	pid = waitpid(-1, &status, wait_flags);
 #endif

commit bb8f6b688a6a8c7ac66b5c656d2544f9167a1abe

ash: fix slow running when background job is present
It was noted that a simple 'while' loop would take considerably
longer to run in a shell which had spawned a background job
compared to one that hadn't.

The problem originates with commit 4d8a277d59 (Implement nonblocking
wait) which introduced a nonblocking wait in ash.

This was found to cause the shell to use 100% of CPU when running
'sleep 60'.  This was 'fixed' by commit 88b8cb61e (ash: Add a very
small timeout to nonblocking wait).

Subsequently commit 96c9c0044 (ash: allow waitpid_child to block)
fixed a problem with the logic of the original commit, but it left
the small timeout in place.  It is this timeout which slows down
the shell when a background job is present.

The solution is to restore the 0 timeout for the nonblocking wait.

Saves 0-16 bytes.

(GitHub issue #434)

shell/ash.c
@@ -4960,7 +4960,7 @@ waitpid_child(int *status, int wait_flags)
 		goto done;
 
 	idx = WaitForMultipleObjects(pid_nr, proclist, FALSE,
-				wait_flags&WNOHANG ? 1 : INFINITE);
+				wait_flags & WNOHANG ? 0 : INFINITE);
 	if (idx < pid_nr) {
 		GetExitCodeProcess(proclist[idx], &win_status);
 		*status = exit_code_to_wait_status(win_status);