rmyorston/busybox-w32

commit 0475b7a649209487269e35cbe49662a33524e6ae

win32: convert exit codes
Add two utility functions to convert Windows process exit codes.

- exit_code_to_wait_status() converts to a POSIX wait status.
  This is used in ash and the implementations of system(3) and
  mingw_wait3().

- exit_code_to_posix() converts to a POSIX exit code.  (Not that
  POSIX has much to say about them.)

As a result it's possible for more applets to report when child
processes are killed as if by a signal.  'time', 'drop' and 'su -W',
for example.

Adds 64-80 bytes.

win32/process.c
@@ -45,7 +45,7 @@ pid_t mingw_wait3(pid_t pid, int *status, int options, struct rusage *rusage)
 			}
 #endif
 			CloseHandle(proc);
-			*status = code << 8;
+			*status = exit_code_to_wait_status(code);
 			return pid;
 		}
 	}
@@ -407,7 +407,7 @@ static NORETURN void wait_for_child(HANDLE child)
 	SetConsoleCtrlHandler(kill_child_ctrl_handler, TRUE);
 	WaitForSingleObject(child, INFINITE);
 	GetExitCodeProcess(child, &code);
-	exit((int)code);
+	exit(exit_code_to_posix(code));
 }
 
 int
@@ -805,6 +805,39 @@ static int kill_SIGKILL(pid_t pid, int sig)
 int FAST_FUNC is_valid_signal(int number)
 {
 	return isalpha(*get_signame(number));
+}
+
+int exit_code_to_wait_status(DWORD exit_code)
+{
+	int sig, status;
+
+	if (exit_code == 0xc0000005)
+		return SIGSEGV;
+	else if (exit_code == 0xc000013a)
+		return SIGINT;
+
+	// When a process is terminated as if by a signal the Windows
+	// exit code is zero apart from the signal in its topmost byte.
+	// This is a busybox-w32 convention.
+	sig = exit_code >> 24;
+	if (sig != 0 && exit_code == sig << 24 && is_valid_signal(sig))
+		return sig;
+
+	// Use least significant byte as exit code, but not if it's zero
+	// and the Windows exit code as a whole is non-zero.
+	status = exit_code & 0xff;
+	if (exit_code != 0 && status == 0)
+		status = 255;
+	return status << 8;
+}
+
+int exit_code_to_posix(DWORD exit_code)
+{
+	int status = exit_code_to_wait_status(exit_code);
+
+	if (WIFSIGNALED(status))
+		return 128 + WTERMSIG(status);
+	return WEXITSTATUS(status);
 }
 
 int kill(pid_t pid, int sig)

include/mingw.h
@@ -562,6 +562,8 @@ int mingw_execve(const char *cmd, char *const *argv, char *const *envp);
 BOOL WINAPI kill_child_ctrl_handler(DWORD dwCtrlType);
 int kill_signal_by_handle(HANDLE process, int sig);
 int FAST_FUNC is_valid_signal(int number);
+int exit_code_to_wait_status(DWORD win_exit_code);
+int exit_code_to_posix(DWORD win_exit_code);
 
 #define find_mount_point(n, s) find_mount_point(n)
 

loginutils/suw32.c
@@ -112,6 +112,8 @@ int suw32_main(int argc UNUSED_PARAM, char **argv)
 		WaitForSingleObject(info.hProcess, INFINITE);
 		if (!GetExitCodeProcess(info.hProcess, &r))
 			r = 1;
+		else
+			r = exit_code_to_posix(r);
 		CloseHandle(info.hProcess);
 		return r;
 	}

miscutils/drop.c
@@ -200,7 +200,7 @@ int drop_main(int argc, char **argv)
 			SetConsoleCtrlHandler(kill_child_ctrl_handler, TRUE);
 			WaitForSingleObject(pi.hProcess, INFINITE);
 			if (GetExitCodeProcess(pi.hProcess, &code)) {
-				return (int)code;
+				return exit_code_to_posix(code);
 			}
 		}
 	}

shell/ash.c
@@ -4960,7 +4960,7 @@ waitpid_child(int *status, int wait_flags)
 	HANDLE *proclist;
 	pid_t pid = -1;
 	DWORD win_status, idx;
-	int i, sig;
+	int i;
 
 	for (jb = curjob; jb; jb = jb->prev_job) {
 		if (jb->state != JOBDONE)
@@ -4992,18 +4992,7 @@ waitpid_child(int *status, int wait_flags)
 				wait_flags&WNOHANG ? 1 : INFINITE);
 	if (idx < pid_nr) {
 		GetExitCodeProcess(proclist[idx], &win_status);
-		if (win_status == 0xc0000005)
-			win_status = SIGSEGV << 24;
-		else if (win_status == 0xc000013a)
-			win_status = SIGINT << 24;
-
-		// When a process is terminated as if by a signal the exit
-		// code is zero apart from the signal in its topmost byte.
-		sig = win_status >> 24;
-		if (sig != 0 && win_status == sig << 24 && is_valid_signal(sig))
-			*status = sig;
-		else
-			*status = (int)win_status << 8;
+		*status = exit_code_to_wait_status(win_status);
 		pid = pidlist[idx];
 	}
  done:

win32/system.c
@@ -6,7 +6,6 @@ int mingw_system(const char *cmd)
 	intptr_t proc;
 	HANDLE h;
 	DWORD ret = 0;
-	int sig;
 
 	if (cmd == NULL)
 		return 1;
@@ -19,9 +18,5 @@ int mingw_system(const char *cmd)
 	GetExitCodeProcess(h, &ret);
 	CloseHandle(h);
 
-	// Was process terminated as if by a signal?
-	sig = ret >> 24;
-	if (sig != 0 && ret == sig << 24 && is_valid_signal(sig))
-		return sig;
-	return ret << 8;
+	return exit_code_to_wait_status(ret);
 }

commit 4bdeee55c1c2a456cae12abdff775fc60903bd6f

win32: fix problem interrupting shell loop
It proved to be almost impossible to interrupt a loop like:

   while true; do sleep 1; done

where 'sleep' was an external program, not an applet.  The issue
was introduced by commit 0475b7a64 (win32: convert exit codes).
This passed a POSIX error code to the exit() in wait_for_child()
so a parent was unable to detect when its child was interrupted.

Pass the Windows exit code to exit() instead.  Work around the
changes introduced by commit 790e377273 (win32: revert 'don't set
error mode').

Adds 16-32 bytes.

win32/process.c
@@ -482,24 +482,22 @@ static int exit_code_to_wait_status_cmd(DWORD exit_code, const char *cmd)
 	return status << 8;
 }
 
-static int exit_code_to_posix_cmd(DWORD exit_code, const char *cmd)
-{
-	int status = exit_code_to_wait_status_cmd(exit_code, cmd);
-
-	if (WIFSIGNALED(status))
-		return 128 + WTERMSIG(status);
-	return WEXITSTATUS(status);
-}
-
 static NORETURN void wait_for_child(HANDLE child, const char *cmd)
 {
 	DWORD code;
+	int status;
 
 	kill_child_ctrl_handler(GetProcessId(child));
 	SetConsoleCtrlHandler(kill_child_ctrl_handler, TRUE);
 	WaitForSingleObject(child, INFINITE);
 	GetExitCodeProcess(child, &code);
-	exit(exit_code_to_posix_cmd(code, cmd));
+	// We don't need the wait status, but get it anyway so the error
+	// message can include the command.  In such cases we pass the
+	// exit status to exit() so our caller won't repeat the message.
+	status = exit_code_to_wait_status_cmd(code, cmd);
+	if (!WIFSIGNALED(status) && code > 0xff)
+		code = WEXITSTATUS(status);
+	exit((int)code);
 }
 
 int
@@ -949,5 +947,9 @@ int exit_code_to_wait_status(DWORD exit_code)
 
 int exit_code_to_posix(DWORD exit_code)
 {
-	return exit_code_to_posix_cmd(exit_code, NULL);
+	int status = exit_code_to_wait_status(exit_code);
+
+	if (WIFSIGNALED(status))
+		return 128 + WTERMSIG(status);
+	return WEXITSTATUS(status);
 }