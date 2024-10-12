rmyorston/busybox-w32

commit 569de936abb90f4c7cdca9da111a6ea780b135bf

kill: killing a zombie process should fail
A process which has exited may still have its process handle
held open by its children.  Such a process doesn't appear in
the process table.  It is thus similar to a zombie process in
UNIX.  Using kill(1) to interact with such a process was seen
to succeed, contrary to expectation.

The code for "ordinary" signals in kill(2) did check if the
process was still active but didn't treat an attempt to kill
an inactive process as an error.  Furthermore, sending SIGKILL
or the fake signal 0 to a process didn't even check if the
process was still active.

Rearrange the implementation of kill(2) so that an attempt to
signal an inactive process is treated as an error.  This also
consolidates handling of SIGKILL and signal 0 with "ordinary"
signals.

Saves 96 bytes.

(GitHub issue #416)

win32/process.c
@@ -685,14 +685,98 @@ void FAST_FUNC read_cmdline(char *buf, int col, unsigned pid, const char *comm)
 }
 
 /**
+ * Determine whether a process runs in the same architecture as the current
+ * one. That test is required before we assume that GetProcAddress() returns
+ * a valid address *for the target process*.
+ */
+static inline int process_architecture_matches_current(HANDLE process)
+{
+	static BOOL current_is_wow = -1;
+	BOOL is_wow;
+
+	if (current_is_wow == -1 &&
+	    !IsWow64Process (GetCurrentProcess(), &current_is_wow))
+		current_is_wow = -2;
+	if (current_is_wow == -2)
+		return 0; /* could not determine current process' WoW-ness */
+	if (!IsWow64Process (process, &is_wow))
+		return 0; /* cannot determine */
+	return is_wow == current_is_wow;
+}
+
+/**
+ * This function tries to terminate a Win32 process, as gently as possible,
+ * by injecting a thread that calls ExitProcess().
+ *
+ * Note: as kernel32.dll is loaded before any process, the other process and
+ * this process will have ExitProcess() at the same address.
+ *
+ * The idea comes from the Dr Dobb's article "A Safer Alternative to
+ * TerminateProcess()" by Andrew Tucker (July 1, 1999),
+ * http://www.drdobbs.com/a-safer-alternative-to-terminateprocess/184416547
+ *
+ */
+static int kill_signal_by_handle(HANDLE process, int sig)
+{
+	DECLARE_PROC_ADDR(DWORD, ExitProcess, LPVOID);
+	PVOID arg = (PVOID)(intptr_t)(sig << 24);
+	DWORD thread_id;
+	HANDLE thread;
+
+	if (!INIT_PROC_ADDR(kernel32, ExitProcess) ||
+			!process_architecture_matches_current(process)) {
+		SetLastError(ERROR_ACCESS_DENIED);
+		return -1;
+	}
+
+	if (sig != 0 && (thread = CreateRemoteThread(process, NULL, 0,
+					   ExitProcess, arg, 0, &thread_id))) {
+		CloseHandle(thread);
+	}
+	return 0;
+}
+
+static int kill_signal(pid_t pid, int sig)
+{
+	HANDLE process;
+	int ret = 0;
+	DWORD code;
+
+	if (sig == SIGKILL)
+		process = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+	else
+		process = OpenProcess(SYNCHRONIZE | PROCESS_CREATE_THREAD |
+			PROCESS_QUERY_INFORMATION |
+			PROCESS_VM_OPERATION | PROCESS_VM_WRITE |
+			PROCESS_VM_READ, FALSE, pid);
+
+	if (!process)
+		return -1;
+
+	if (!GetExitCodeProcess(process, &code) || code != STILL_ACTIVE) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		ret = -1;
+	} else if (sig == SIGKILL) {
+		/* This way of terminating processes is not gentle: they get no
+		 * chance to clean up after themselves (closing file handles,
+		 * removing .lock files, terminating spawned processes (if any),
+		 * etc). */
+		ret = !TerminateProcess(process, SIGKILL << 24);
+	} else {
+		ret = kill_signal_by_handle(process, sig);
+	}
+	CloseHandle(process);
+
+	return ret;
+}
+
+/**
  * If the process ID is positive invoke the callback for that process
  * only.  If negative or zero invoke the callback for all descendants
  * of the indicated process.  Zero indicates the current process; negative
  * indicates the process with process ID -pid.
  */
-typedef int (*kill_callback)(pid_t pid, int sig);
-
-static int kill_pids(pid_t pid, int sig, kill_callback killer)
+static int kill_pids(pid_t pid, int sig)
 {
 	DWORD pids[16384];
 	int max_len = sizeof(pids) / sizeof(*pids), i, len, ret = 0;
@@ -750,109 +834,11 @@ static int kill_pids(pid_t pid, int sig, kill_callback killer)
 
 	for (i = len - 1; i >= 0; i--) {
 		SetLastError(0);
-		if (killer(pids[i], sig)) {
+		if (kill_signal(pids[i], sig)) {
 			errno = err_win_to_posix();
 			ret = -1;
 		}
 	}
-
-	return ret;
-}
-
-/**
- * Determine whether a process runs in the same architecture as the current
- * one. That test is required before we assume that GetProcAddress() returns
- * a valid address *for the target process*.
- */
-static inline int process_architecture_matches_current(HANDLE process)
-{
-	static BOOL current_is_wow = -1;
-	BOOL is_wow;
-
-	if (current_is_wow == -1 &&
-	    !IsWow64Process (GetCurrentProcess(), &current_is_wow))
-		current_is_wow = -2;
-	if (current_is_wow == -2)
-		return 0; /* could not determine current process' WoW-ness */
-	if (!IsWow64Process (process, &is_wow))
-		return 0; /* cannot determine */
-	return is_wow == current_is_wow;
-}
-
-/**
- * This function tries to terminate a Win32 process, as gently as possible,
- * by injecting a thread that calls ExitProcess().
- *
- * Note: as kernel32.dll is loaded before any process, the other process and
- * this process will have ExitProcess() at the same address.
- *
- * The idea comes from the Dr Dobb's article "A Safer Alternative to
- * TerminateProcess()" by Andrew Tucker (July 1, 1999),
- * http://www.drdobbs.com/a-safer-alternative-to-terminateprocess/184416547
- *
- */
-int kill_signal_by_handle(HANDLE process, int sig)
-{
-	DWORD code;
-	int ret = 0;
-
-	if (GetExitCodeProcess(process, &code) && code == STILL_ACTIVE) {
-		DECLARE_PROC_ADDR(DWORD, ExitProcess, LPVOID);
-		PVOID arg = (PVOID)(intptr_t)(sig << 24);
-		DWORD thread_id;
-		HANDLE thread;
-
-		if (!INIT_PROC_ADDR(kernel32, ExitProcess) ||
-				!process_architecture_matches_current(process)) {
-			SetLastError(ERROR_ACCESS_DENIED);
-			ret = -1;
-			goto finish;
-		}
-
-		if ((thread = CreateRemoteThread(process, NULL, 0,
-					    ExitProcess, arg, 0, &thread_id))) {
-			CloseHandle(thread);
-		}
-	}
-
- finish:
-	CloseHandle(process);
-	return ret;
-}
-
-static int kill_signal(pid_t pid, int sig)
-{
-	HANDLE process;
-
-	if (!(process = OpenProcess(SYNCHRONIZE | PROCESS_CREATE_THREAD |
-			PROCESS_QUERY_INFORMATION |
-			PROCESS_VM_OPERATION | PROCESS_VM_WRITE |
-			PROCESS_VM_READ, FALSE, pid))) {
-		return -1;
-	}
-
-	return kill_signal_by_handle(process, sig);
-}
-
-/*
- * This way of terminating processes is not gentle: they get no chance to
- * clean up after themselves (closing file handles, removing .lock files,
- * terminating spawned processes (if any), etc).
- *
- * If the signal isn't SIGKILL just check if the target process exists.
- */
-static int kill_SIGKILL(pid_t pid, int sig)
-{
-	HANDLE process;
-	int ret = 0;
-
-	if (!(process=OpenProcess(PROCESS_TERMINATE, FALSE, pid))) {
-		return -1;
-	}
-
-	if (sig == SIGKILL)
-		ret = !TerminateProcess(process, SIGKILL << 24);
-	CloseHandle(process);
 
 	return ret;
 }
@@ -897,10 +883,8 @@ int exit_code_to_posix(DWORD exit_code)
 
 int kill(pid_t pid, int sig)
 {
-	if (sig == SIGKILL || sig == 0)
-		return kill_pids(pid, sig, kill_SIGKILL);
-	else if (is_valid_signal(sig))
-		return kill_pids(pid, sig, kill_signal);
+	if (is_valid_signal(sig))
+		return kill_pids(pid, sig);
 
 	errno = EINVAL;
 	return -1;

coreutils/timeout.c
@@ -54,7 +54,9 @@ static HANDLE child = INVALID_HANDLE_VALUE;
 static void kill_child(void)
 {
 	if (child != INVALID_HANDLE_VALUE) {
-		kill_signal_by_handle(child, SIGTERM);
+		pid_t pid = (pid_t)GetProcessId(child);
+		if (pid)
+			kill(pid, SIGTERM);
 	}
 }
 
@@ -206,13 +208,15 @@ int timeout_main(int argc UNUSED_PARAM, char **argv)
 	status = signo == SIGKILL ? 137 : 124;
 
 	pid = (pid_t)GetProcessId(child);
-	kill(pid, signo);
+	if (pid) {
+		kill(pid, signo);
 
-	if (kill_timeout > 0) {
-		if (timeout_wait(kill_timeout, child, &status))
-			goto finish;
-		kill(pid, SIGKILL);
-		status = 137;
+		if (kill_timeout > 0) {
+			if (timeout_wait(kill_timeout, child, &status))
+				goto finish;
+			kill(pid, SIGKILL);
+			status = 137;
+		}
 	}
  finish:
 	CloseHandle(child);

include/mingw.h
@@ -569,7 +569,6 @@ int mingw_execve(const char *cmd, char *const *argv, char *const *envp);
 #define has_dos_drive_prefix(path) (isalpha(*(path)) && (path)[1] == ':')
 
 BOOL WINAPI kill_child_ctrl_handler(DWORD dwCtrlType);
-int kill_signal_by_handle(HANDLE process, int sig);
 int FAST_FUNC is_valid_signal(int number);
 int exit_code_to_wait_status(DWORD win_exit_code);
 int exit_code_to_posix(DWORD win_exit_code);

commit 8db590e2431a18628a693d49e943d2c4d94cb85c

kill: fix regression in 'kill -9'
'kill -9' was found to fail with an 'Invalid argument' error.

This is a regression introduced by commit 569de936a (kill: killing
a zombie process should fail).

Use the correct argument to OpenProcess() for SIGKILL so it can
query the exit code of the target process.

Adds 16 bytes.

(GitHub issue #465)

win32/process.c
@@ -822,15 +822,16 @@ static int kill_signal(pid_t pid, int sig)
 {
 	HANDLE process;
 	int ret = 0;
-	DWORD code;
+	DWORD code, flags;
 
 	if (sig == SIGKILL)
-		process = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+		flags = PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION;
 	else
-		process = OpenProcess(SYNCHRONIZE | PROCESS_CREATE_THREAD |
-			PROCESS_QUERY_INFORMATION |
-			PROCESS_VM_OPERATION | PROCESS_VM_WRITE |
-			PROCESS_VM_READ, FALSE, pid);
+		flags = SYNCHRONIZE | PROCESS_CREATE_THREAD |
+					PROCESS_QUERY_INFORMATION |
+					PROCESS_VM_OPERATION | PROCESS_VM_WRITE |
+					PROCESS_VM_READ;
+	process = OpenProcess(flags, FALSE, pid);
 
 	if (!process)
 		return -1;