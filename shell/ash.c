rmyorston/busybox-w32

commit bb8f6b688a6a8c7ac66b5c656d2544f9167a1abe

ash: fix slow running when background job is present
It was noted that a simple 'while' loop would take considerably
longer to run in a shell which had spawned a background job
compared to one that hadn't.

The problem originates with commit 4d8a277d59 (Implement nonblocking
wait) which introduced a nonblocking wait in ash.

This was found to cause the shell to use 100% of CPU when running
'sleep 60'.  This was 'fixed' by commit 88b8cb61e (ash: Add a very
small timeout to nonblocking wait).

Subsequently commit 96c9c0044 (ash: allow waitpid_child to block)
fixed a problem with the logic of the original commit, but it left
the small timeout in place.  It is this timeout which slows down
the shell when a background job is present.

The solution is to restore the 0 timeout for the nonblocking wait.

Saves 0-16 bytes.

(GitHub issue #434)

shell/ash.c
@@ -4960,7 +4960,7 @@ waitpid_child(int *status, int wait_flags)
 		goto done;
 
 	idx = WaitForMultipleObjects(pid_nr, proclist, FALSE,
-				wait_flags&WNOHANG ? 1 : INFINITE);
+				wait_flags & WNOHANG ? 0 : INFINITE);
 	if (idx < pid_nr) {
 		GetExitCodeProcess(proclist[idx], &win_status);
 		*status = exit_code_to_wait_status(win_status);

commit f6be217fa12f794d225788e4a62ce60fd709c266

ash: wait builtin should block
It was found that the 'wait' shell builtin would use 100% of a core
when waiting for a process to terminate:

   sleep 60 &
   wait

This is a regression caused by commit bb8f6b688 (ash: fix slow
running when background job is present).  See the commit message
and GitHub issue #434 for the long and involved history.

The problem is in the Windows implementation of waitproc() in ash.
The 'block' argument to waitproc() can take three values:

   DOWAIT_NONBLOCK
   DOWAIT_BLOCK
   DOWAIT_CHILD_OR_SIG

The first two have obvious meanings.  The third performs a non-
blocking wait(2) and if no PID is returned it waits for SIGCHLD.
So in effect it's a blocking wait.

The Windows implementation would perform a non-blocking wait(2)
but couldn't then wait for SIGCHLD, because Windows doesn't have
such a signal.  As a result the 'wait' builtin would loop calling
waitproc().

To avoid this DOWAIT_CHILD_OR_SIG should be treated as a blocking
wait in waitproc().

(GitHub issue #529)

shell/ash.c
@@ -5142,7 +5142,7 @@ waitproc(int block, int *status)
 
 	return err;
 #else
-	int flags = block == DOWAIT_BLOCK ? 0 : WNOHANG;
+	int flags = block == DOWAIT_NONBLOCK ? WNOHANG : 0;
 	*status = 0;
 	return waitpid(-1, status, flags);
 #endif