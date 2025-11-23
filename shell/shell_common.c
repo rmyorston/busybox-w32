rmyorston/busybox-w32

commit 8e699173370d68262273390e1b631300d9c5017c

ash: fix 'read' shell built-in (1)
Consider this test case:

   { echo -n te; sleep 3; echo st; } | (read -t 1 x; echo "$x")

- bash echoes "te" after 1 second.

- Upstream BusyBox echoes an empty "$x" after 1 second.

- busybox-w32 echoes an empty "$x" after 3 seconds.

The delayed echo in busybox-w32 arises because the 'read' shell
built-in omits the code to poll for input.

Rearrange the code so that polling takes place.  This doesn't address
the difference between BusyBox and bash.

Costs 48-64 bytes.

shell/shell_common.c
@@ -195,7 +195,6 @@ shell_builtin_read(struct builtin_read_params *params)
 			}
 		}
 
-#if !ENABLE_PLATFORM_MINGW32
 		/* We must poll even if timeout is -1:
 		 * we want to be interrupted if signal arrives,
 		 * regardless of SA_RESTART-ness of that signal!
@@ -209,13 +208,7 @@ shell_builtin_read(struct builtin_read_params *params)
 			retval = (const char *)(uintptr_t)1;
 			goto ret;
 		}
-		if (read(fd, &buffer[bufpos], 1) != 1) {
-			err = errno;
-			retval = (const char *)(uintptr_t)1;
-			break;
-		}
-#else
-		errno = 0;
+#if ENABLE_PLATFORM_MINGW32
 		if (isatty(fd)) {
 			int64_t key;
 
@@ -245,15 +238,13 @@ shell_builtin_read(struct builtin_read_params *params)
 				/* echo input if not in silent mode */
 				console_write(buffer + bufpos, 1);
 			}
-		}
-		else {
-			if (read(fd, &buffer[bufpos], 1) != 1) {
-				err = errno;
-				retval = (const char *)(uintptr_t)1;
-				break;
-			}
-		}
+		} else
 #endif
+		if (read(fd, &buffer[bufpos], 1) != 1) {
+			err = errno;
+			retval = (const char *)(uintptr_t)1;
+			break;
+		}
 
 		c = buffer[bufpos];
 #if ENABLE_PLATFORM_MINGW32

commit 42c3b212d9f9fe964401ed45b1cfd4b67066fb80

ash: allow ctrl-c to interrupt read in mintty
The command 'read -t 10' couldn't be interrupted by ctrl-c
when ash was running in the mintty terminal emulator.

This issue was introduced by commits 8e6991733 and b2901ce8e
which fixed other problems with the 'read' builtin.

Rearrange the code to avoid calling poll(2) in an interactive
shell on Windows.

(GitHub issue #547)

shell/shell_common.c
@@ -222,24 +222,6 @@ shell_builtin_read(struct builtin_read_params *params)
 		pfd->events = POLLIN;
 
 #if ENABLE_PLATFORM_MINGW32
-		/* Don't poll if timeout is -1, it hurts performance.  The
-		 * caution above about interrupts isn't relevant on Windows
-		 * where Ctrl-C causes an event, not a signal.
-		 */
-		if (timeout >= 0)
-#endif
-		/* test bb_got_signal, then poll(), atomically wrt signals */
-		if (check_got_signal_and_poll(pfd, timeout) <= 0) {
-			/* timed out, or some error */
-			err = errno;
-			if (!err) { /* timed out */
-				retval = (const char *)(uintptr_t)2;
-				break;
-			}
-			retval = (const char *)(uintptr_t)1;
-			goto ret;
-		}
-#if ENABLE_PLATFORM_MINGW32
 		if (isatty(fd)) {
 			int64_t key;
 
@@ -272,13 +254,32 @@ shell_builtin_read(struct builtin_read_params *params)
 				/* echo input if not in silent mode */
 				console_write(buffer + bufpos, 1);
 			}
-		} else
+		} else {
+			/* Don't poll if timeout is -1, it hurts performance.  The
+			 * caution above about interrupts isn't relevant on Windows
+			 * where Ctrl-C causes an event, not a signal.
+			 */
+			if (timeout >= 0)
 #endif
+		/* test bb_got_signal, then poll(), atomically wrt signals */
+		if (check_got_signal_and_poll(pfd, timeout) <= 0) {
+			/* timed out, or some error */
+			err = errno;
+			if (!err) { /* timed out */
+				retval = (const char *)(uintptr_t)2;
+				break;
+			}
+			retval = (const char *)(uintptr_t)1;
+			goto ret;
+		}
 		if (read(fd, &buffer[bufpos], 1) != 1) {
 			err = errno;
 			retval = (const char *)(uintptr_t)1;
 			break;
 		}
+#if ENABLE_PLATFORM_MINGW32
+		}
+#endif
 
 		c = buffer[bufpos];
 #if ENABLE_PLATFORM_MINGW32