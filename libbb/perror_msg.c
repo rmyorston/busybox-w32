0956c787b2299f58c01504171aa7d52256f8867e
Ron Yorston <rmy@pobox.com>
2026-09-18 09:13:36 +0100

    ping: code shrink WSA error messages
    Generation of WSA error messages has been moved into a separate
    function to avoid duplication.
    
    The new function manages the allocated buffer so the callers
    don't need to free it.
    
    Saves 16-56 bytes.
    
    (GitHub PR #608)
    
    Signed-off-by: Ron Yorston <rmy@pobox.com>

--- libbb/perror_msg.c
+++ libbb/perror_msg.c
@@ -8,63 +8,53 @@
  */
 #include "libbb.h"
 
+#if ENABLE_PLATFORM_MINGW32
+/* Add the error number to WSA errors, since strerror doesn't support them.
+ * The result may be in an allocated buffer.  If the caller needs to
+ * preserve this, it should take a copy. */
+static char *strerror_wsa(void)
+{
+	char *errname = NULL;
+	static char *wsa_error = NULL;
+
+	// The range is from 995 to 11031
+	// https://learn.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
+	if (errno >= WSA_OPERATION_ABORTED && errno <= WSA_QOS_RESERVED_PETYPE) {
+		free(wsa_error);
+		errname = wsa_error = xasprintf("%d: WSA error", errno);
+	} else if (errno) {
+		errname = strerror(errno);
+	}
+	return errname;
+}
+#endif
+
 void bb_perror_msg(const char *s, ...)
 {
+	va_list p;
+
+	va_start(p, s);
+	/* Guard against "<error message>: Success" */
 #if ENABLE_PLATFORM_MINGW32
-	va_list p;
-	char *errname = NULL;
-	char *freeme = NULL;
-	/* We add the error number to WSA errors, since strerror doesn't support them.
-	   This has to be done here, since we need the resulting buffer to be freed,
-	   and there is no way in mingw_strerror to do that without needing to
-	   involve thread-locals */
-	/* we have the range from 995 to 11031 */
-	/* see https://learn.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2 */
-	if (errno >= WSA_OPERATION_ABORTED && errno <= WSA_QOS_RESERVED_PETYPE)
-		errname = freeme = xasprintf("%d: WSA error", errno);
-	else if (errno)
-		errname = strerror(errno);
-
-	va_start(p, s);
-	/* Guard against "<error message>: Success" */
-	bb_verror_msg(s, p, errname);
-	va_end(p);
-	free(freeme);
+	bb_verror_msg(s, p, strerror_wsa());
 #else
-	va_list p;
-
-	va_start(p, s);
-	/* Guard against "<error message>: Success" */
 	bb_verror_msg(s, p, errno ? strerror(errno) : NULL);
-	va_end(p);
 #endif
+	va_end(p);
 }
 
 void bb_perror_msg_and_die(const char *s, ...)
 {
+	va_list p;
+
+	va_start(p, s);
+	/* Guard against "<error message>: Success" */
 #if ENABLE_PLATFORM_MINGW32
-	va_list p;
-	char *errname = NULL;
-	char *freeme = NULL;
-	/* see above */
-	if (errno >= WSA_OPERATION_ABORTED && errno <= WSA_QOS_RESERVED_PETYPE)
-		errname = freeme = xasprintf("%u: WSA error", errno);
-	else if (errno)
-		errname = strerror(errno);
-
-	va_start(p, s);
-	/* Guard against "<error message>: Success" */
-	bb_verror_msg(s, p, errname);
-	va_end(p);
-	free(freeme);
+	bb_verror_msg(s, p, strerror_wsa());
 #else
-	va_list p;
-
-	va_start(p, s);
-	/* Guard against "<error message>: Success" */
 	bb_verror_msg(s, p, errno ? strerror(errno) : NULL);
-	va_end(p);
 #endif
+	va_end(p);
 	xfunc_die();
 }
 
